6379a91fd6365eceea33f1c73643fc55fffa5145
Hemanth <133361769+PANYAMHEMANTH@users.noreply.github.com>
2024-09-04 15:11:20 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,10 +1,4 @@
-// example.js
-
-// Define a global function
 function showAlert(message) {
-    alert(hiiii);
-  }
-  
-  // Export the function to make it accessible globally
-  window.showAlert = showAlert;
-  
+  alert(message);
+}
+window.showAlert = showAlert;
